aed8e3af00a59644a350bfa9ba1b2b41a7de1484
Dervieux <dervieux.ben@gmail.com>
2022-04-30 11:54:45 +0200

    Creation of 2 components: Banner and Cart

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Banner from "./Banner";
+import "../styles/App.css";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Banner() {
+  const title = "La maison Jungle";
+  return (
+    <div>
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+export default Banner;
